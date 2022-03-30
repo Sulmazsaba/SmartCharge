dafa31f262bcd1090c4aba1368de650c8d0d2218
Sulmaz <Sulmaz@DESKTOP-MM1IDAG>
2022-03-30 14:14:04 +0430

    added GroupRepository Tests

---
+++ SmartCharge.Infrastructure.Tests/ConnectorRepositoryTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartCharge.Infrastructure.Tests
+{
+    public class ConnectorRepositoryTests
+    {
+
+        [Fact]
+        public async Task 
+    }
+}
--- SmartCharge.Infrastructure.Tests/GroupRepositoryTests.cs
+++ SmartCharge.Infrastructure.Tests/GroupRepositoryTests.cs
@@ -1,13 +1,61 @@
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using SmartCharge.Domain.Entities;
+using SmartCharge.Infrastructure.Persistence;
+using SmartCharge.Infrastructure.Repositories;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SmartCharge.Infrastructure.Tests
 {
     public class GroupRepositoryTests
     {
+
         [Fact]
-        public void Test1()
+        public async Task FindGroupById_ShouldReturnCorrectValue()
         {
 
+            var dbOptions = new DbContextOptionsBuilder<SmartChargeContext>().UseInMemoryDatabase("FindGroupByIdTest").Options;
+
+            using var context = new SmartChargeContext(dbOptions);
+
+            var groupRepository = new GroupRepository(context);
+
+            var group = new Group
+            {
+                Id = 1,
+                CapacityInAmps = 2,
+                Name = "test"
+            };
+
+            await groupRepository.AddAsync(group);
+
+            var result =await groupRepository.FindById(group.Id);
+
+            result.Id.Should().NotBe(null);
+            result.CapacityInAmps.Should().Be(group.CapacityInAmps);
+            result.Name.Should().Be(group.Name);
         }
+
+        [Fact]
+        public async Task FindGroupById_WhenInvalidId_ShouldReturnNull()
+        {
+            var dbOptions = new DbContextOptionsBuilder<SmartChargeContext>().UseInMemoryDatabase("FindGroupByInvalidIdTest").Options;
+
+            using var context = new SmartChargeContext(dbOptions);
+
+            var groupRepository = new GroupRepository(context); var group = new Group
+            {
+                Id = 1,
+                CapacityInAmps = 2,
+                Name = "test"
+            };
+
+            await groupRepository.AddAsync(group);
+            var result = await groupRepository.FindById(2);
+            result.Should().BeNull();
+
+        }
+
     }
 }
